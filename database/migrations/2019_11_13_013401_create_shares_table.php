f246dcd270a627a2fd5cdc89b73e1d4f9639790f
Pavel <Pavel>
2019-11-13 04:37:09 +0300

    Initial

---
+++ database/migrations/2019_11_13_013401_create_shares_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSharesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shares', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('photo_id');
+            $table->bigInteger('user_from');
+            $table->bigInteger('user_to');
+            $table->timestamps();
+        });
+
+        Schema::table('shares', function (Blueprint $table) {
+            $table->index(['photo_id', 'user_from', 'user_to']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shares');
+    }
+}
